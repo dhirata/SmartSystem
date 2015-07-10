1ac15911e014083e97457595896807cf1c47d64d
Derek Hirata <dhirata@andrew.cmu.edu>
2015-07-10 16:08:55 -0400

    Added relationships and validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+	# Use built-in rails support for password protection
+  	has_secure_password
+
+  	# Relationships
+  	has_one :customer
+
+
+	# Validations
+	validates :username, presence: true, uniqueness: { case_sensitive: false}
+	validates_inclusion_of :role, :in => %w[admin customer], message: "is not a recognized role in system"
+	validates_presence_of :password, on: :create 
+	validates_presence_of :password_confirmation, on: :create 
+	validates_confirmation_of :password, on: :create, message: "does not match"
+	validates_length_of :password, minimum: 4, message: "must be at least 4 characters long", allow_blank: true
+
+	
 end
