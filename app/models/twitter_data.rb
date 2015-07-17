e2cf2218cdfc718bbf5ea7ac3038ec0e250dd71a
Derek Hirata <dhirata@andrew.cmu.edu>
2015-07-17 11:54:26 -0400

    Added validations, relationships, and scopes to twitter data model

--- app/models/twitter_data.rb
+++ app/models/twitter_data.rb
@@ -1,2 +1,16 @@
 class TwitterData < ActiveRecord::Base
+	# Relationship
+	has_one :company
+
+	#Validations
+	validates_date :data_date
+	validates_numericality_of :favorites, greater_than_or_equal_to: 0, only_integer: true
+	validates_numericality_of :retweets, greater_than_or_equal_to: 0, only_integer: true
+	validates_numericality_of :followers, greater_than_or_equal_to: 0, only_integer: true
+	validates_numericality_of :polarity, greater_than_or_equal_to: 0
+
+	#Scopes
+	scope :by_retweets, -> { order(:retweets) }
+	scope :by_followers, -> { order(:followers) }
+	scope :chronological, -> { order(data_date: :desc) }
 end
