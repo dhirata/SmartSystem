f1d589ad8598028cc0138819b1fabc31bc75282d
Derek Hirata <dhirata@andrew.cmu.edu>
2015-07-24 00:10:43 -0400

    Added relationships and validations to financial data model

--- app/models/financial_data.rb
+++ app/models/financial_data.rb
@@ -1,2 +1,9 @@
-class FinancialData < ActiveRecord::Base
+class FinancialData < ActiveRecord::Base 
+	#relationship
+	belongs_to :company
+
+	#validations
+	validates_date :data_date
+	validates_numericality_of :stock_price, greater_than_or_equal_to: 0
+
 end
