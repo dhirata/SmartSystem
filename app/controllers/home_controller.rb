527630a58b6927f497b8846a2cd4974835c6fd0e
Derek Hirata <dhirata@andrew.cmu.edu>
2015-07-24 16:18:42 -0400

    Added home controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+	def home
+		@smarties = Smarty.chronological.paginate(page: params[:page]).per_page(25)
+	end
+
+	def about
+	end
+
+	def privacy
+	end
+
+	def contact
+	end
+end
