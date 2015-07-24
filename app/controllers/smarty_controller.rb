6a6a9581cdeb67ee33e7cb8ec1ab6a1a21db7105
Derek Hirata <dhirata@andrew.cmu.edu>
2015-07-24 01:10:53 -0400

    set a smarty for show page and stuff in smarty controller

--- app/controllers/smarty_controller.rb
+++ app/controllers/smarty_controller.rb
@@ -1,2 +1,13 @@
 class SmartyController < ApplicationController
+	before_action :set_smarty, only: [:show, :edit, :update, :destroy]
+
+
+	private
+	def set_smarty
+		@smarty = Smarty.find(params[:id])
+	end
+
+	def item_params
+    	params.require(:smarty).permit(:customer_id, :description, :analysis, :company_id)
+  	end
 end
