fc7229040cf27272886eae3ea70ffc4df7ae1f1e
Derek Hirata <dhirata@andrew.cmu.edu>
2015-07-24 00:47:51 -0400

    Added relationships and validations to smarty model

--- app/models/smarty.rb
+++ app/models/smarty.rb
@@ -1,2 +1,8 @@
 class Smarty < ActiveRecord::Base
+	#relationships
+	belongs_to :customer
+	has_many :companies
+
+	#validations
+	validates_inclusion_of :analysis, :in => %w[buy sell stay], message: "not a recommendation"
 end
