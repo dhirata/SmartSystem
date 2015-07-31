6ea00c391555ed0b1c1896b24d868be4d6deda2c
Derek Hirata <dhirata@andrew.cmu.edu>
2015-07-31 01:48:09 -0400

    home controller stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 	def home
 		@smarties = Smarty.chronological.paginate(page: params[:page]).per_page(25)
 		@companies = Company.by_name.paginate(page: params[:page]).per_page(25)
+		@company_count = @companies.size
 	end
 
 	def about
