c0b92279e157e6a8e8e626215f6f65cda97aa210
Derek Hirata <dhirata@andrew.cmu.edu>
2015-08-07 14:33:48 -0400

    Category dropdown work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,10 @@ class HomeController < ApplicationController
 		@smarties = Smarty.chronological.paginate(page: params[:page]).per_page(25)
 		@companies = Company.by_name.paginate(page: params[:page]).per_page(25)
 		@company_count = @companies.size
-		@filter_params = Set.new
+		@filter_company_params = Set.new
+		@filter_category_params = Set.new
 		@all_options = ["Buy", "Sell", "Hold"]
+		@all_categories = ["Product", "Executive", "Acquisition"]
 	end
 
 	def about
