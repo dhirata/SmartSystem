72408bc2a3ebe86421be135be4acc64c9db22e8f
Derek Hirata <dhirata@andrew.cmu.edu>
2015-08-07 02:28:25 -0400

    Improved visual appearance

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
 		@smarties = Smarty.chronological.paginate(page: params[:page]).per_page(25)
 		@companies = Company.by_name.paginate(page: params[:page]).per_page(25)
 		@company_count = @companies.size
+		@filter_params = Set.new
 	end
 
 	def about
