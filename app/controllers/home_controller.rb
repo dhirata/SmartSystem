0c024a3fdc58f9b21544cd7e22341cd41481e2cb
Derek Hirata <dhirata@andrew.cmu.edu>
2015-08-11 02:19:13 -0400

    Getting rid of nav flag

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
 	def home
-		@nav_flag = true
 		@smarties = Smarty.chronological.paginate(page: params[:page]).per_page(25)
 		@companies = Company.by_name.paginate(page: params[:page]).per_page(25)
 		@company_count = @companies.size
@@ -11,7 +10,6 @@ class HomeController < ApplicationController
 	end
 
 	def login
-		@nav_flag = false
 	end
 
 	def filterCompany
